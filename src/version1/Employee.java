aaae4d99b66f8e53a38c800d6efc0127d9d4574e
dev <devarriesgado@gmail.com>
2026-09-08 12:23:43 +0800

    Initialize Project

---
+++ src/version1/Employee.java
@@ -0,0 +1,64 @@
+package version1;
+
+public class HourlyEmployee {
+
+    private int empID;
+    private String empName;
+    private float totalHoursWorked;
+    private double ratePerHour;
+
+    public HourlyEmployee() {
+        this.empID = 0;
+        this.empName = "N/A";
+        this.ratePerHour = 0;
+        this.totalHoursWorked = 0;
+    }
+
+
+    public HourlyEmployee(int empID, String empName) {
+        this.empID = empID;
+        this.empName = empName;
+        this.ratePerHour = 0;
+        this.totalHoursWorked = 0;
+    }
+
+    public HourlyEmployee(int empID, String empName, float totalHoursWorked, double ratePerHour) {
+        this.empID = empID;
+        this.empName = empName;
+        setRatePerHour(ratePerHour);
+        setTotalHoursWorked(totalHoursWorked);
+    }
+
+    public int getEmpID() {
+        return empID;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public float getTotalHoursWorked() {
+        return totalHoursWorked;
+    }
+
+    public double getRatePerHour() {
+        return ratePerHour;
+    }
+
+    public void setEmpID(int empID) {
+        this.empID = empID;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public void setTotalHoursWorked(float totalHoursWorked) {
+        this.totalHoursWorked = totalHoursWorked;
+    }
+
+    public void setRatePerHour(double ratePerHour) {
+        this.ratePerHour = ratePerHour;
+    }
+
+}
